6b438447235479d202b8300a5c4b16114c03c2fb
nurbek <abduhamidovnurbek888@gmail.com>
2022-04-08 18:29:20 +0500

    Pizza

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Image from "next/image";
+import Featured from "../components/Featured";
+import PizzaCard from "../components/PizzaCard";
+import PizzaList from "../components/PizzaList";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza Restaurant in Uzbekistan</title>
+        <meta name="description" content="Best pizza shop in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Featured />
+      <PizzaList/>
+    </div>
+  );
+}
